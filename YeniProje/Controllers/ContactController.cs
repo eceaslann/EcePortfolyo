bf3c61064530448c6e94959c34126f66b24eef43
Ece <Ece@DESKTOP-RCI54VP>
2023-04-17 11:31:59 +0300

    Proje dosyası ekle.

---
+++ YeniProje/Controllers/ContactController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using YeniProje.Models;
+
+namespace YeniProje.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        DbResumeEntities db = new DbResumeEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            List<SelectListItem> values = (from x in db.TblCategory.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v = values;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SendMessage(TblContact p)
+        {
+            p.Date = DateTime.Parse(DateTime.Now.ToShortDateString());
+            db.TblContact.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Default");
+        }
+        
+        
+    }
+}
---
+++ YeniProje/Controllers/DefaultController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using YeniProje.Models;
+
+namespace ResumeProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DbResumeEntities db = new DbResumeEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            var values = db.TblProfile.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialService()
+        {
+            var values = db.TblService.ToList();
+            return PartialView(values);
+
+        }
+        public PartialViewResult PartialFeaturedSkill()
+        {
+            var values = db.TblTechnology.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialBanner() { return PartialView();}
+        public PartialViewResult PartialChoose() { return PartialView(); }
+        public PartialViewResult PartialStats()
+        {
+            ViewBag.skillCount = db.TblSkill.Count();
+            ViewBag.serviceCount = db.TblService.Count();
+            ViewBag.averageTechnologyValue = db.TblTechnology.Average(x => x.TechnologyValue);
+            ViewBag.happyCustomer = 38;
+            return PartialView();
+        }
+        public PartialViewResult PartialPortfolyo()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialBrands() { return PartialView(); }
+        public PartialViewResult PartialFooter() { return PartialView(); }
+        public PartialViewResult PartialScripts() { return PartialView(); }
+    }
+}
